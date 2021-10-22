30a7f04d73c0ff038bdc186c78f75e24de37d98a
Nerdyxils <silasabiodun25@gmail.com>
2021-10-22 16:30:44 +0100

    contact form progress

--- src/Components/signIn-Components/contactComponent.jsx
+++ src/Components/signIn-Components/contactComponent.jsx
@@ -1,22 +1,95 @@
 import React from 'react'
-
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import './contact.style.css'
 
-class contactInput extends React.Component {
-    constructor(props) {
-        super (props)
-        this.state = {
-            name: '',
-            email: '',
-            title: '',
-            message: ''
-        }
-    }
-    render() { 
-        return <div>
+const ContactComponent = () => {
+    const formik = useFormik({
+        initialValues: {
+          name: '',
+          email: '',
+          messageTitle: '',
+          message: ''
+        },
+        validationSchema: Yup.object({
+          name: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          messageTitle: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          message: Yup.string()
+            .max(20, 'Must be 20 characters or less')
+            .required('Required'),
+          email: Yup.string().email('Invalid email address').required('Required'),
+        }),
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+        },
+      });
 
-        </div>;
-    }
+
+      return (
+        <form onSubmit={formik.handleSubmit}>
+          {/* <label htmlFor="name">First Name</label> */}
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <div>{formik.errors.name}</div>
+          ) : null}
+
+        {/* <label htmlFor="email">Email Address</label> */}
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Email Address"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+    
+          {/* <label htmlFor="lastName">Last Name</label> */}
+          <input
+            id="messageTitle"
+            name="messageTitle"
+            type="text"
+            placeholder="Title"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.messageTitle}
+          />
+          {formik.touched.messageTitle && formik.errors.messageTitle ? (
+            <div>{formik.errors.messageTitle}</div>
+          ) : null}
+
+          {/* <label htmlFor="lastName">Last Name</label> */}
+          <input
+            id="message"
+            name="message"
+            type="text"
+            placeholder="Message"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.message}
+          />
+          {formik.touched.message && formik.errors.message ? (
+            <div>{formik.errors.message}</div>
+          ) : null}
+    
+          <button type="submit">Submit</button>
+        </form>
+      );
 }
  
-export default contactInput;
+export default ContactComponent;
--- src/Pages/contact/contact.jsx
+++ src/Pages/contact/contact.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import ContactComponent from '../../Components/signIn-Components/contactComponent';
 
 import './contact.css'
 
 const contact = () => {
     return (
         <div className="page_container">
-            <h2 style={{color: 'white'}}>Contact</h2>
+            <h2 style={{color: 'white', marginBottom: '30px'}}>Contact</h2>
+            <ContactComponent />
         </div>
     )
 }
