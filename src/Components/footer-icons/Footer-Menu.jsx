6ceb85d8b7fec42a179ad2d5cad6c4b30c307b40
Nerdyxils <silasabiodun25@gmail.com>
2021-10-26 09:20:20 +0100

    added more contact info

--- src/Components/footer-icons/Footer-Menu.jsx
+++ src/Components/footer-icons/Footer-Menu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ExternalLink } from 'react-external-link'
-import { FaTwitterSquare, FaGithubSquare, FaLinkedin } from 'react-icons/fa'
+import { FaTwitterSquare, FaGithubSquare, FaLinkedin, FaPhoneSquare, FaSkype } from 'react-icons/fa'
 import './Footer-Menu.css'
 
 export default function FooterMenu() {
@@ -15,6 +15,12 @@ export default function FooterMenu() {
                 </ExternalLink>
                 <ExternalLink href="https://www.linkedin.com/in/abiodun-silas-timi/">
                     <FaLinkedin className="icon" />
+                </ExternalLink>
+                <ExternalLink href="https://join.skype.com/invite/dWU2ZilFUvjc">
+                    <FaSkype className="icon" />
+                </ExternalLink>
+                <ExternalLink href="tel:+2349043378175">
+                    <FaPhoneSquare className="icon" />
                 </ExternalLink>
             </div>
         </div>
