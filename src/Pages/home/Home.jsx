9208f9d1d12f98f02a76cb3091b9f2560fbd3299
Nerdyxils <silasabiodun25@gmail.com>
2025-03-20 17:32:51 -0400

    updated package and home page

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { motion } from 'framer-motion';
-import Typing from 'react-typing-animation';
-import './Home.css'
+import { TypeAnimation } from 'react-type-animation';
+import './Home.css';
 
 const pageVariants = {
     initial: { opacity: 0, scale: 0.8, rotateX: 90 },
@@ -22,13 +22,7 @@ const pageVariants = {
         rotateX: -90,
         transition: { duration: 0.5 }
     }
-}
-
-const homeVariants = {
-    initial: { opacity: 0, scale: 0.5, rotateY: 180 },
-    in: { opacity: 1, scale: 1, rotateY: 0 },
-    out: { opacity: 0, scale: 1.5, rotateY: -180 },
-  };
+};
 
 const Home = () => (
     <motion.div
@@ -75,17 +69,34 @@ const Home = () => (
                             transition: { delay: 0.6 }
                         }}
                     >
-                        <Typing speed={50}>
-                            <span className="alt-txt">A Web Developer</span>
-                            <Typing.Backspace count={13} delay={900} />
-                            <span className="alt-txt">ReactJs Developer</span>
-                            <Typing.Backspace count={17} delay={900} />
-                            <span className="alt-txt">Wordpress Developer</span>
-                            <Typing.Backspace count={19} delay={900} />
-                            <span className="alt-txt">Project Manager</span>
-                            <Typing.Backspace count={16} delay={900} />
-                            <span className="alt-txt">Front End Developer</span>
-                        </Typing>
+                        <TypeAnimation
+                            sequence={[
+                                'A Web Developer',      // Text 1
+                                1500,                  // Wait 1.5s
+                                '',                    // Backspace to empty
+                                500,                   // Wait 0.5s before next
+                                'A ReactJs Developer',   // Text 2
+                                1500,                  // Wait 1.5s
+                                '',                    // Backspace
+                                500,                   // Wait 0.5s
+                                'A Wordpress Developer', // Text 3
+                                1500,                  // Wait 1.5s
+                                '',                    // Backspace
+                                500,                   // Wait 0.5s
+                                'A Project Manager',     // Text 4
+                                1500,                  // Wait 1.5s
+                                '',                    // Backspace
+                                500,                   // Wait 0.5s
+                                'A ReactJs Developer', // Text 5
+                                1500,                  // Wait 1.5s
+                                '',                    // Backspace
+                                500                    // Wait 0.5s before loop
+                            ]}
+                            wrapper="span"
+                            speed={50}                // Typing speed (ms per character)
+                            repeat={Infinity}         // Loop forever
+                            className="alt-txt"       // Apply your existing CSS class
+                        />
                     </motion.div>
                     <motion.h1 
                         className="txt-2"
@@ -100,11 +111,12 @@ const Home = () => (
                             }
                         }}
                     >
-                        based remotely in Ontario, Canada.
+                        based in Ontario, Canada.
                     </motion.h1>
                 </div>
             </div>
         </div>
     </motion.div>
-)
-export default Home
+);
+
+export default Home;
